9a7bb883fed69b6f875fe4bd17684f79e2459943
nakarid <hamdal@gmail.com>
2017-03-07 17:26:27 -0500

    dropdown

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-alert("estoy aqui!!!")
+//funcion Dropdown de Michelle.
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+myFunction()
