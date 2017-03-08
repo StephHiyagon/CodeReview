3d9d18e98fc511e0f672eea41d663f381d7053fd
itsandromeda <itsandromeda@gmail.com>
2017-03-08 00:13:14 -0500

    Refactorizado

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,13 +1,17 @@
-/*global prompt*/
+/*global prompt, alert, document, window*/
 /*global name:true */
-var sprintOne = document.getElementById("sprint-I"),
-    sprintTwo = document.getElementById("sprint-II"),
-    sprintThree = document.getElementById("sprint-III"),
-    sprintContent = document.getElementById("sprint-content"),
-    n, //name of user - username
-    i,
+var n, //name of user - username
     username = document.getElementById("username"),
-    openDropdown;
+    openDropdown,
+    mostarOcultar,
+    divtext1,
+    divtext2,
+    divtext,
+    cont,
+    quizResp1,
+    mostrar,
+    area,
+    i;
 
 function name() {
     "use strict";
@@ -22,60 +26,82 @@ function menu() {
 	document.getElementById("myDropdown").classList.toggle("show");
 }
 //FUNCIONALIDAD BOTONES
-var mostarOcultar = function(divtexte) {
-	switch(divtexte){
-		case "text1": var divtext1 = document.getElementById("text2");
-									var divtext2 = document.getElementById("text3");
-									break;
-		case "text2": var divtext1 = document.getElementById("text1");
-									var divtext2 = document.getElementById("text3");
-									break;
-		case "text3": var divtext1 = document.getElementById("text1");
-									var divtext2 = document.getElementById("text2");
-									break;
+mostarOcultar = function (divtexte) {
+    "use strict";
+	switch (divtexte) {
+    case "text1":
+        divtext1 = document.getElementById("text2");
+        divtext2 = document.getElementById("text3");
+        break;
+    case "text2":
+        divtext1 = document.getElementById("text1");
+        divtext2 = document.getElementById("text3");
+        break;
+    case "text3":
+        divtext1 = document.getElementById("text1");
+        divtext2 = document.getElementById("text2");
+        break;
 	}
-	var divtext = document.getElementById(divtexte);
+    
+	divtext = document.getElementById(divtexte);
 
-	if (divtext.style.display == "none" ) {
-			divtext1.style.display = "none";
-			divtext2.style.display = "none";
-			divtext.style.display = "block";
-	} else{
+	if (divtext.style.display === "none") {
+        divtext1.style.display = "none";
+        divtext2.style.display = "none";
+        divtext.style.display = "block";
+	} else {
 		divtext.style.display = "block";
 	}
-}
-var quizResp1= function(area, mostrar) {
-	 var cont=0;
-	 var mostrar = document.getElementById(mostrar);
-	 if((document.getElementById('1aux11').checked==false&& document.getElementById('1aux12').checked==false &&document.getElementById('1ok1').checked==false) ||
-	   (document.getElementById('1aux21').checked==false && document.getElementById('1aux22').checked==false && document.getElementById('1ok2').checked==false) ||
-		  (document.getElementById('1aux31').checked==false && document.getElementById('1aux32').checked==false && document.getElementById('1ok3').checked==false)){
-				alert("Responder todas las preguntas");
-	 }else{
-		 if(document.getElementById('ok1').checked==true){cont++;};
-		 if(document.getElementById('ok2').checked==true){cont++;};
-		 if(document.getElementById('ok3').checked==true){cont++;};
-		 var area = document.getElementById(area);
-		 mostrar.innerHTML = "Tienes "+ cont + " correcta(s).";
-		 area.style.display = "none";
-		}
-}
-var quizResp2= function(area, mostrar) {
-	 var cont=0;
-	 var mostrar = document.getElementById(mostrar);
-	 if((document.getElementById('aux11').checked==false&& document.getElementById('aux12').checked==false &&document.getElementById('ok1').checked==false)||
-	   (document.getElementById('aux21').checked==false && document.getElementById('aux22').checked==false && document.getElementById('ok2').checked==false) ||
-		  (document.getElementById('aux31').checked==false && document.getElementById('aux32').checked==false && document.getElementById('ok3').checked==false)){
-				alert("Responda todas las preguntas");
-	 }else{
-		 if(document.getElementById('ok1').checked==true){cont++;};
-		 if(document.getElementById('ok2').checked==true){cont++;};
-		 if(document.getElementById('ok3').checked==true){cont++;};
-		 var area = document.getElementById(area);
-		 mostrar.innerHTML = "Tienes "+ cont + " correcta(s).";
-		 area.style.display = "none";
-		}
-}
+};
+
+quizResp1 = function (area, mostrar) {
+    "use strict";
+	cont = 0;
+	mostrar = document.getElementById(mostrar);
+    
+	if ((document.getElementById('1aux11').checked === false &&
+        document.getElementById('1aux12').checked === false &&
+        document.getElementById('1ok1').checked === false) || (document.getElementById('1aux21').checked === false &&
+        document.getElementById('1aux22').checked === false &&
+        document.getElementById('1ok2').checked === false) || (document.getElementById('1aux31').checked === false &&
+        document.getElementById('1aux32').checked === false &&
+        document.getElementById('1ok3').checked === false)) {
+        alert("Responder todas las preguntas");
+    } else if (document.getElementById('ok1').checked === true) {
+        cont += 1;
+    } else if (document.getElementById('ok2').checked === true) {
+        cont += 1;
+    } else if (document.getElementById('ok3').checked === true) {
+        cont += 1;
+    }
+    
+	area = document.getElementById(area);
+    mostrar.innerHTML = "Tienes " + cont + " correcta(s).";
+	area.style.display = "none";
+};
+
+var quizResp2 = function (area, mostrar) {
+    "use strict";
+    cont = 0;
+	mostrar = document.getElementById(mostrar);
+    
+    if ((document.getElementById('aux11').checked === false &&
+         document.getElementById('aux12').checked === false &&
+         document.getElementById('ok1').checked === false) || (document.getElementById('aux21').checked === false && document.getElementById('aux22').checked === false &&
+         document.getElementById('ok2').checked === false) || (document.getElementById('aux31').checked === false && document.getElementById('aux32').checked === false &&
+         document.getElementById('ok3').checked === false)) {
+        alert("Responda todas las preguntas");
+    } else if (document.getElementById('ok1').checked === true) {
+        cont += 1;
+    } else if (document.getElementById('ok2').checked === true) {
+        cont += 1;
+    } else if (document.getElementById('ok3').checked === true) {
+        cont += 1;
+    }
+    area = document.getElementById(area);
+    mostrar.innerHTML = "Tienes " + cont + " correcta(s).";
+    area.style.display = "none";
+};
 //FUNCIONALIDAD BOTONES
 name();
 
